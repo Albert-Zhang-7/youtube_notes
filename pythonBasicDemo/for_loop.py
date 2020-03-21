c4c21ba9df4a3000659e3008a84061b8a1023ece
Albert-Zhang-7 <18260089775@163.com>
2020-03-21 20:43:20 +0800

    first version

---
+++ pythonBasicDemo/for_loop.py
@@ -0,0 +1,14 @@
+my_list = ["jake", "Tom", "xi li"]
+
+# for name in my_list:
+#     print(name)
+
+# for number in range(11):
+#     print(number)
+
+
+for letter in 'inty':
+    print("each letter is: " + letter)
+
+'''这是github的demo'''
+"I also want to have a try."
